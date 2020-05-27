7feb93478bc1698c95d6818ec7d867d63b4438ff
APFirebolt <aspper20@gmail.com>
2020-05-27 12:26:45 -0700

    Adding models to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from . models import BlogPostImages, BlogCategory, BlogPost, FamousQuotes
+from . models import BlogPostImages, BlogCategory, BlogPost, FamousQuotes, UncategorizedBlog, UnclassifiedBlogImages
 
 
 admin.site.register(BlogCategory)
 admin.site.register(BlogPostImages)
 admin.site.register(BlogPost)
 admin.site.register(FamousQuotes)
+admin.site.register(UncategorizedBlog)
+admin.site.register(UnclassifiedBlogImages)
